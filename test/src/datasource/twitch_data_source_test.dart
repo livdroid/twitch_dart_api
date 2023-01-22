3dcf3538a182bdc66b3d5f5cd702a3cf444431a6
LivDroid <oliviarodrigue@gmail.com>
2023-01-22 10:11:15 +0100

    Add put unit test

--- test/src/datasource/twitch_data_source_test.dart
+++ test/src/datasource/twitch_data_source_test.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
-import 'package:mockito/mockito.dart';
 import 'package:twitch_client/src/datasource/twitch_data_source.dart';
 import 'package:twitch_client/src/error/exceptions.dart';
 import 'package:twitch_client/src/utils/url_constants.dart';
@@ -773,6 +772,200 @@ void main() {
             path: fullPath,
             queryParams: BitsLeaderBoardProps().toJson(),
             data: BitsLeaderBoardProps().toJson()),
+        throwsA(isA<ServerException>()),
+      );
+    });
+  });
+
+  group('Twitch Data Source - PUT', () {
+    final apiResponse = assetReader('response_bitsleaderboard.json');
+    const path = 'bits/leaderboard';
+    String fullPath = '$_baseURL$path';
+
+    test('Call is successfull an return a value', () async {
+      _dioAdapter.onPut(
+        fullPath,
+        data: {},
+        queryParameters: BitsLeaderBoardProps().toJson(),
+        (request) => request.reply(200, apiResponse),
+      );
+
+      final response = await datasource.put(
+          path: fullPath,
+          queryParams: BitsLeaderBoardProps().toJson(),
+          data: {});
+
+      expect(response, apiResponse);
+    });
+
+    test('Call is successfull an return a 204', () async {
+      _dioAdapter.onPut(
+        fullPath,
+        data: {},
+        queryParameters: BitsLeaderBoardProps().toJson(),
+        (request) => request.reply(204, true),
+      );
+
+      final response = await datasource.put(
+          path: fullPath,
+          queryParams: BitsLeaderBoardProps().toJson(),
+          data: {});
+
+      expect(response, isA<bool>());
+      expect(response, true);
+    });
+
+    test('Request is not authorized an return a 403 error', () async {
+      _dioAdapter.onPut(
+        fullPath,
+        data: {},
+        queryParameters: BitsLeaderBoardProps().toJson(),
+        (request) => request.throws(
+          403,
+          DioError(
+            response: Response(
+                statusCode: 403,
+                requestOptions: RequestOptions(
+                  baseUrl: _baseURL,
+                  path: path,
+                )),
+            requestOptions: RequestOptions(
+              baseUrl: _baseURL,
+              path: path,
+            ),
+          ),
+        ),
+      );
+
+      expect(
+        () async => await datasource.put(
+            path: fullPath,
+            queryParams: BitsLeaderBoardProps().toJson(),
+            data: {}),
+        throwsA(isA<ForbiddenRequestException>()),
+      );
+    });
+
+    test('Request is not correct an return a 400 error', () async {
+      _dioAdapter.onPut(
+        fullPath,
+        data: {},
+        queryParameters: BitsLeaderBoardProps().toJson(),
+        (request) => request.throws(
+          400,
+          DioError(
+            response: Response(
+                statusCode: 400,
+                requestOptions: RequestOptions(
+                  baseUrl: _baseURL,
+                  path: path,
+                )),
+            requestOptions: RequestOptions(
+              baseUrl: _baseURL,
+              path: path,
+            ),
+          ),
+        ),
+      );
+
+      expect(
+        () async => await datasource.put(
+            path: fullPath,
+            queryParams: BitsLeaderBoardProps().toJson(),
+            data: {}),
+        throwsA(isA<BadRequestException>()),
+      );
+    });
+
+    test('Request is not auth an return a 401 error', () async {
+      _dioAdapter.onPut(
+        fullPath,
+        data: {},
+        queryParameters: BitsLeaderBoardProps().toJson(),
+        (request) => request.throws(
+          401,
+          DioError(
+            response: Response(
+                statusCode: 401,
+                requestOptions: RequestOptions(
+                  baseUrl: _baseURL,
+                  path: path,
+                )),
+            requestOptions: RequestOptions(
+              baseUrl: _baseURL,
+              path: path,
+            ),
+          ),
+        ),
+      );
+
+      expect(
+        () async => await datasource.put(
+            path: fullPath,
+            queryParams: BitsLeaderBoardProps().toJson(),
+            data: {}),
+        throwsA(isA<UnauthorizedException>()),
+      );
+    });
+
+    test('An unknown error 430 was returned', () async {
+      _dioAdapter.onPut(
+        fullPath,
+        data: {},
+        queryParameters: BitsLeaderBoardProps().toJson(),
+        (request) => request.throws(
+          430,
+          DioError(
+            response: Response(
+                statusCode: 430,
+                requestOptions: RequestOptions(
+                  baseUrl: _baseURL,
+                  path: path,
+                )),
+            requestOptions: RequestOptions(
+              baseUrl: _baseURL,
+              path: path,
+            ),
+          ),
+        ),
+      );
+
+      expect(
+        () async => await datasource.put(
+            path: fullPath,
+            queryParams: BitsLeaderBoardProps().toJson(),
+            data: {}),
+        throwsA(isA<Exception>()),
+      );
+    });
+
+    test('An error 500 was returned', () async {
+      _dioAdapter.onPut(
+        fullPath,
+        data: {},
+        queryParameters: BitsLeaderBoardProps().toJson(),
+        (request) => request.throws(
+          500,
+          DioError(
+            response: Response(
+                statusCode: 500,
+                requestOptions: RequestOptions(
+                  baseUrl: _baseURL,
+                  path: path,
+                )),
+            requestOptions: RequestOptions(
+              baseUrl: _baseURL,
+              path: path,
+            ),
+          ),
+        ),
+      );
+
+      expect(
+        () async => await datasource.put(
+            path: fullPath,
+            queryParams: BitsLeaderBoardProps().toJson(),
+            data: {}),
         throwsA(isA<ServerException>()),
       );
     });
