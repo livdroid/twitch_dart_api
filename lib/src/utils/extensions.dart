f3d9efda60a1ce26bada7a9cbedbed3d70e884a3
LivDroid <oliviarodrigue@gmail.com>
2023-01-29 22:52:57 +0100

    clean unused extension

--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -1,11 +1,5 @@
 import 'package:dartz/dartz.dart';
 
-extension EitherHelpers<L, R> on Either<L, R> {
-  R? unwrapRight() {
-    return toOption().toNullable();
-  }
-}
-
 extension EitherX<L, R> on Either<L, R> {
   R asRight() => (this as Right).value; //
   L asLeft() => (this as Left).value;
