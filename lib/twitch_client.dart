3ce918d69111ce59b31a9ca69086b092777e8b1f
Olivia <oliviarodrigue@gmail.com>
2023-04-01 20:01:21 +0200

    Export props

--- lib/twitch_client.dart
+++ lib/twitch_client.dart
@@ -9,6 +9,8 @@ export 'src/props/add_vip_props.dart';
 export 'src/props/ban_user_data_props.dart';
 export 'src/props/ban_user_props.dart';
 export 'src/props/bits_leaderboard_props.dart';
+export 'src/props/create_clip_props.dart';
+export 'src/props/get_clip_props.dart';
 export 'src/props/block_user_props.dart';
 export 'src/props/broadcaster_id_props.dart';
 export 'src/props/broadcaster_moderator_props.dart';
